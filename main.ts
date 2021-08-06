3a9a0ef5ea5333ec2b5f75d6f80c7daf74188ed3
YomamaChunky <87711048+YomamaChunky@users.noreply.github.com>
2021-08-06 10:38:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
+basic.forever(function () {
+	
+})
